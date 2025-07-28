45daca6d553a0737fd9c2de0d39eb1cbc825cd2f
Daniel Anifowoshe <danielanifowoshe04@gmail.com>
2025-07-28 17:53:59 +0100

    feat: enhance tab layout with additional screens and icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { images } from '@/assets/constants/images'
+import { icons } from '@/assets/constants/icons'
 
 const _Layout = () => {
   return (
@@ -9,8 +11,39 @@ const _Layout = () => {
            name='index'
            options={{
             title: 'Home',
-            headerShown: false
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+                <>
+                    <ImageBackground 
+                        source={images.highlight}
+                    >
+                        <Image source={icons.home} tintColor="#151312" className='size-5' />
+                        <Text>Home</Text>
+                    </ImageBackground>
+                </>
+            )
            }}
+        />
+        <Tabs.Screen
+            name='search'
+            options={{
+                headerShown: false,
+                title: 'Search'
+            }}
+        />
+        <Tabs.Screen
+            name='saved'
+            options={{
+                headerShown: false,
+                title: 'Saved'
+            }}
+        />
+        <Tabs.Screen
+            name='profile'
+            options={{
+                headerShown: false,
+                title: 'Profile'
+            }}
         />
     </Tabs>
   )
