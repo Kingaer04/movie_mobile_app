71cb12b2a6fa57cc33c8174ddb603f69b88a89a0
Daniel Anifowoshe <danielanifowoshe04@gmail.com>
2025-08-01 17:11:23 +0100

    feat: add trending movies fetching and update search count functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,33 +6,51 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useEffect";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
 
 export default function Index() {
   const router = useRouter();
+
+  const { data: trendingMovies, loading: trendingLoading, error: trendingError } = useFetch(getTrendingMovies);
   
   const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }))
+
 
   // Header component to render at the top of FlatList
   const renderHeader = () => (
     <>
       <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-      {moviesLoading ? (
+      {moviesLoading || trendingLoading? (
         <ActivityIndicator
           size="large"
           color="#0000ff"
           className="mt-10 self-center"
         />
-      ) : moviesError ? (
-        <Text>Error: {moviesError?.message}</Text>
+      ) : moviesError || trendingError ? (
+        <Text>Error: {moviesError?.message || trendingError?.message}</Text>
       ) : (
         <View className="mt-5">
           <SearchBar
-            value=""
-            onChangeText={() => {}}
             onPress={() => router.push("/search")}
             placeholder="Search for movies, series, etc."
           />
-          <Text className="text-white text-lg font-bold mt-5 mb-3">Trending Movies</Text>
+          { trendingMovies && (
+            <View className="mt-10">
+              <Text className="text-white text-lg font-bold mt-5 mb-3">Trending Movies</Text>
+            </View>
+          )}
+          <FlatList
+            className="mt-3 mb-4"
+            data={trendingMovies}
+            renderItem={({ item, index }) => (
+              <Text className="text-white text-sm">{item.title}</Text>
+            )}
+            keyExtractor={(item) => item.movie_id.toString()}
+            // horizontal
+            // showsHorizontalScrollIndicator={false}
+            // contentContainerStyle={{ paddingHorizontal: 15 }}
+          />
+          <Text className="text-white text-lg font-bold mt-5 mb-3">Popular Movies</Text>
         </View>
       )}
     </>
